7873367122ee0acab425608584cbb9f6b485257e
Josh Cox <josh@webhosting.coop>
2015-12-19 18:55:35 -0600

    build-essential

--- bootstrapbase.sh
+++ bootstrapbase.sh
@@ -3,7 +3,7 @@ TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
 
 cd $TMP
 echo local-base|tee $TMP/helloworld
-apt-get install -y wget unzip
+apt-get install -y wget unzip build-essential
 wget -c https://github.com/joshuacox/local-base/archive/master.zip 
 unzip master.zip
 cd local-base-master
